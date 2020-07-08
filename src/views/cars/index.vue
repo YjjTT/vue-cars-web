3649aab42eb358b86234b6bce7100833eddb6794
YjjTT <yjjtt@tempot.com>
2020-07-08 23:56:27 +0800

    cars 列表item完成

--- src/views/cars/index.vue
+++ src/views/cars/index.vue
@@ -8,8 +8,36 @@
                 </h4>
                 <p class="cars-attr">新能源汽车 5座</p>
             </header>
-            <div class="cars-content"></div>
-            <footer></footer>
+            <div class="cars-content">
+                <div class="info">
+                    <div>
+                        <h4 class="cars-number">粤 213213</h4>
+                        <div>
+                            <ul class="cars-electric active-li-6">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <p class="distance">
+                                <sub>约</sub>
+                                <strong>600</strong>
+                                <sub>KM</sub>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <img src="../../assets/images/pic001.jpg" alt="">
+            </div>
+            <footer>
+                <a href="javascript:;" class="parking-link">某某停车场</a>
+            </footer>
         </section>
         <span @click="user">汽车列表</span>
     </div>
@@ -41,11 +69,11 @@
         background-color: #ffffff;
         border-radius: 24px;
         margin: auto;
-        padding: 30px;
+        padding: 30px 30px 0;
         @include webkit(box-shadow, 0 0 18px 0 rgba(0,0,0,0.2));
         @include webkit(box-sizing, border-box);
         width: 505px;
-        height: 207px;
+        height: 257px;
         header {
             display: flex;
             h4, p {
@@ -66,5 +94,80 @@
         text-align: right;
         opacity: 0.5;
         line-height: 34px;
+    }
+    .cars-content {
+        position: relative;
+        @include clearfix;
+        margin: 11px 0 7px 0;
+        img {
+            float: right;
+            height: 120px;
+        }
+        .info {
+            display: flex;
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            align-items: center;
+        }
+        .cars-number {
+            font-size: 18px;
+        }
+        .cars-electric {
+            display: inline-block;
+            padding-top: 10px;
+            li {
+                display: inline-block;
+                width: 7px;
+                height: 24px;
+                border-radius: 100px;
+                background-color: #e1e1e1;
+                margin-right: 3px;
+            }
+            @for $i from 1 through 10 {
+                &.active-li-#{$i} {
+                    li:nth-child(-n+#{$i}) {
+                        @include webkitA(background, linear-gradient(#17a8fa, #108dd9));
+                    }
+                }
+            }
+        }
+        .distance {
+            margin-left: 5px;
+            display: inline-block;
+            strong {
+                margin: 0 3px;
+                font-size: 22px;
+            }
+            sub {
+                position: relative;
+                top: -2px;
+            }
+            sub:first-of-type {
+                opacity: .8;
+            }
+        }
+    }
+    .parking-link {
+        display: block;
+        border-top: 1px solid #e1e1e1;
+        color: $color_main;
+        opacity: .8;
+        line-height: 53px;
+        position: relative;
+        &:after {
+            content: '';
+            position: absolute;
+            top: 50%;
+            margin-top: -8px;
+            border-top: 1px solid #d6d7d9;
+            border-right: 1px solid #d6d7d9;
+            width: 8px;
+            height: 8px;
+            right: 0;
+            @include webkit(transform, rotate(45deg));
+        }
     }
 </style>
